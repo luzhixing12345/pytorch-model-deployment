18a2dec2ffe31175862a86060d42f86d01e7da5e
luzhixing12345 <luzhixing12345@163.com>
2022-06-15 12:00:15 +0800

    [feat] : add basic login pages

---
+++ static/index.js
@@ -0,0 +1,5 @@
+function getInputValue(){
+    var first_name = document.getElementById("first_name").value;
+    var last_name = document.getElementById("last_name").value;
+    alert("Hello " + first_name + " " + last_name);
+}
