3bf71c851c41c542762a2239ad8f5082c3cafb9f
luzhixing12345 <luzhixing12345@163.com>
2022-07-03 09:13:34 +0800

    [feat] : finish web server

--- app.py
+++ app.py
@@ -1,34 +1,80 @@
 
-from flask import render_template,request
+
+from flask import redirect, render_template,request,make_response, url_for
 from flask import Flask
 import torch
-from torchvision import transforms,utils
+from torchvision import utils
 from model import load_model
-import os
+import json
+import re
 
 app = Flask(__name__)
+app.secret_key = 'fkdjsafjdkfdlkjfadskjfadskljdsfklj'
+
 G = load_model()
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index1.html',image_url="./static/fake_images.png",data=0,load = 'false')
+
+# @app.route('/uploads/<path:filename>')
+# def download_file(filename):
+#     return send_from_directory(os.path.join('static','images'),
+#                                filename, as_attachment=True)
+
+
+@app.route('/changePageTo', methods=['GET','POST'])
+def navigateToGenerator():
     
-@app.route('/generate', methods=['GET', 'POST'])
+    image_url = "./static/images/multiply.png"
+    return render_template('index2.html', image_url=image_url)
+
+
+@app.route('/changePageBack', methods=['GET','POST'])
+def navigateBackGenerator():
+    
+    return redirect(url_for('index'))
+
+
+
+@app.route('/generate', methods=['GET','POST'])
 def generate():
     if request.method == 'POST':
-        z = torch.randn(64,100,1,1)
-        
-        fake_images = G(z)
-        fake_images = fake_images.mul(0.5).add(0.5).cpu()
-        # save the fake_images
-        # toPIL = transforms.ToPILImage()
-        # image = toPIL(fake_images[0])
-        # image.save(os.path.join('static','fake_images.png'))
-        
-        image_grid = utils.make_grid(fake_images, nrow=8)
-        utils.save_image(image_grid, './static/fake_images.png')
-        print('Saved fake images')
-        
-    return render_template('index.html', image_url="fake_images.png")
+        result = json.dumps(request.form)
+        # print(result)
+        data = json.loads(result)['jsonval']
+        if data == '':
+            # from index2.html
+            z = torch.randn(64,100,1,1)
+            fake_images = G(z)
+            fake_images = fake_images.mul(0.5).add(0.5).cpu()
+            
+            image_grid = utils.make_grid(fake_images, nrow=8)
+            utils.save_image(image_grid, './static/images/multiply.png')
+            print('Saved fake images')
+            image_url = "./static/images/multiply.png"
+            
+            return render_template('index2.html', image_url=image_url)
+        else:
+            # from index1.html
+            data = re.findall(r"\d+\.?\d*",data)
+            z = [(float(i)/255-0.5)/0.5 for i in data]
+            
+            # z = torch.randn(64,100,1,1)
+            # print(torch.max(z),torch.min(z))
+            z = torch.tensor(z).unsqueeze(0).unsqueeze(2).unsqueeze(3)
+            z = [z for _ in range(64)]
+            z = torch.cat(z)
+            # print(z.shape)
+            fake_images = G(z)
+            fake_images = fake_images.mul(0.5).add(0.5).cpu()
+            
+            image_grid = utils.make_grid(fake_images[0], nrow=1)
+            utils.save_image(image_grid, './static/images/single.png')
+            print('Saved fake images')
+            image_url = "./static/images/single.png"
+            
+            return render_template('index1.html', image_url=image_url,data=data,load='true')
     
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0', port=9000)
+    app.run(debug = True, host='0.0.0.0', port=3000)#, threaded = False, processes=50)
--- static/index.js
+++ static/index.js
@@ -1,2 +1,189 @@
-// 获取 (0,1)的高斯分布
+const container = document.getElementById('container')
 
+
+let isDrawing = false;
+let colorTrend = true; // true => darker & false => lighter
+
+
+
+// let basicHeight = document.documentElement.clientHeight;
+// let basicWidth = document.documentElement.clientWidth;
+
+container.addEventListener('mousedown', (e) => {
+    if (e.button == 2) {
+        // right click
+        colorTrend = false;
+    } else if (e.button == 0) {
+        // left click
+        colorTrend = true;
+    }
+    isDrawing = true;
+})
+
+container.addEventListener('mouseup', () => {
+    isDrawing = false;
+})
+
+
+
+
+window.onload = function () {
+    //去掉默认的contextmenu事件，否则会和右键事件同时出现。
+    document.oncontextmenu = function (e) {
+        e.preventDefault();
+    };
+}
+
+// window.onresize = resizeEvent;
+
+
+
+const SQUARES = 100;
+for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover', () => setColor(square));
+    container.appendChild(square);
+}
+
+
+initalizeContainerColor()
+
+function setColor(element) {
+    if (isDrawing) {
+        const color = setNewColor(element);
+        element.style.background = color;
+        element.style.boxshadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+    }
+}
+
+function setNewColor(element) {
+    const color = element.style.background;
+    let newColor;
+    if (colorTrend) {
+        newColor = getDarkColor(color, 0.5);
+    } else {
+        newColor = getLightColor(color, 0.5);
+    }
+    return newColor
+}
+
+function RgbToHex(rgbc) {
+    var hexs = [rgbc[0].toString(16), rgbc[1].toString(16), rgbc[2].toString(16)];
+    for (var i = 0; i < 3; i++) if (hexs[i].length == 1) hexs[i] = "0" + hexs[i];
+    return "#" + hexs.join("");
+}
+
+function RGBRegValue(rgb) {
+    var reg = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)/i;
+    rgb = reg.exec(rgb)
+    return parseInt(rgb[1])
+}
+
+//得到hex颜色值为color的加深颜色值，level为加深的程度，限0-1之间
+function getDarkColor(rgbc, level) {
+    
+    var rgb = [];
+    var color = RGBRegValue(rgbc);
+    for (var i = 0; i < 3; i++) rgb.push(Math.floor(color * (1 - level)));
+    return RgbToHex(rgb);
+}
+//得到hex颜色值为color的减淡颜色值，level为加深的程度，限0-1之间
+function getLightColor(rgbc, level) {
+    var rgb = [];
+    var color = RGBRegValue(rgbc);
+    for (var i = 0; i < 3; i++) rgb.push(Math.floor((255 - color) * level + color));
+    return RgbToHex(rgb);
+}
+
+function initializeSquareColor(square) {
+    let rgbc = [];
+    const color = Math.floor(Math.random() * 256);
+    // 白 -> 黑 rgb三个值相同
+    for (let i = 0; i < 3; i++) {
+        rgbc.push(color)
+    }
+    square.style.background = RgbToHex(rgbc);
+}
+
+function initalizeContainerColor() {
+    // console.log(container.childNodes);
+    var squareData = document.getElementById("data");
+    console.log(squareData);
+    var loadColor = false;
+    var previousColor;
+    if (squareData!=null&&squareData.getAttribute('loaddata') == 'true') {
+        // click generate
+        loadColor = true;
+        previousColor = squareData.getAttribute('datavalue').match(/\d+/g)
+    }
+
+    const childNodes = container.childNodes;
+    for (let i = 0; i < childNodes.length; i++) {
+        if (loadColor) {
+            var color = parseInt(previousColor[i]);
+            var rgb = [color,color,color];
+            childNodes[i].style.background = RgbToHex(rgb);
+        } else {
+            initializeSquareColor(childNodes[i])
+        }
+    }
+    if (squareData!=null) squareData.setAttribute('loaddata','false');
+}
+
+// $(function() {
+//     $('[name=generate]').click(function(){
+//         var box_value = [];
+//         const childNodes = container.childNodes;
+//         for (let i = 0; i<childNodes.length; i++) {
+//             box_value.push(RGBRegValue(childNodes[i].style.background))
+//         }
+//         // console.log(box_value)
+//         $.post('./data', {
+//             data : JSON.stringify(box_value),
+//         })
+//     })
+// })
+
+function getJSON() {
+    var formInfo = document.forms['SUBMIT'];
+    console.log(formInfo);
+    var box_value = [];
+    const childNodes = container.childNodes;
+    for (let i = 0; i<childNodes.length; i++) {
+        box_value.push(RGBRegValue(childNodes[i].style.background))
+    }
+
+    formInfo.jsonval.value = JSON.stringify(box_value);
+    console.log(formInfo);
+}
+
+// function generate() {
+    
+//     $.ajax({
+//         type: "POST",
+//         contentType: 'application/json',
+//         url: "/generate",
+//         dataType: "json",
+//         data : JSON.stringify(box_value),
+//         success:function(response){
+//             console.log(response);
+//             document.write(response); 
+//        }
+
+//     });
+// }
+
+// function resizeEvent() {
+//     var height = document.documentElement.clientHeight;
+//     var width = document.documentElement.clientWidth;
+//     for (let i=0;i<container.childNodes.length;i++) {
+//         var block = container.childNodes[i];
+//         block.style.width *= height/basicHeight;
+//         block.style.height*= width/basicWidth;
+//     }
+// }
+
+function changePage() {
+
+}
